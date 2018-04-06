a67c910bf2ee54e19d6e82061d2c77404317b852
Alexandria Szoeke <alex_szoeke@outlook.com>
2018-04-05 21:38:24 -0500

    incorporated weather API. Currently updating form

---
+++ assets/javascript/weather.js
@@ -0,0 +1,28 @@
+var APIKey = "36c7748bd9dc2a7d1ab2e26004815c20";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+      "q=Houston,Burundi&units=imperial&appid=" + APIKey;
+
+    // Here we run our AJAX call to the OpenWeatherMap API
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // We store all of the retrieved data inside of an object called "response"
+      .then(function(response) {
+
+        // Log the queryURL
+        console.log(queryURL);
+
+        // Log the resulting object
+        console.log(response);
+
+        // Transfer content to HTML
+        $(".houston").html(response.name + " Weather Details");
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".temp").text("Temperature (F) " + response.main.temp);
+
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + response.main.temp);
+      });
